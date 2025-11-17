3700aa4a74bde946be77424683793442cff233da
Taha Khan <tahakhan@Tahas-MacBook-Air.local>
2025-11-18 04:43:37 +0500

    constants changes with details

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,17 +1,17 @@
 class Intro {
   static const name = 'Taha Khan';
-  static const position = 'Flutter Developer | Mobile App Enthusiast';
+  static const position = 'Flutter | Go Lang Developer';
   static const description =
-      'I specialize in building user-friendly mobile applications with Flutter.';
+      'I specialize in building user-friendly mobile applications with Flutter and highly scaleable backends using Go';
 }
 
 class Links {
-  static const github = 'https://github.com/prabeshpudasaini';
+  static const github = 'https://github.com/Tahadxbydy';
   static const githubRepository =
-      'https://github.com/prabeshpudasaini?tab=repositories';
-  static const linkedin = 'https://www.linkedin.com/in/prabesh-pudasaini/';
-  static const phoneNo = '+9779861551377';
-  static const email = 'pudasainiprabesh56@gmail.com';
+      'https://github.com/Tahadxbydy?tab=repositories';
+  static const linkedin = 'https://www.linkedin.com/in/taha-irtaza-a09151244/';
+  static const phoneNo = '+923336750370';
+  static const email = 'tahairtaza121@gmail.com';
   static const resume =
       'https://prabesh.pages.dev/Prabesh_Pudasaini_Flutter_Developer_Resume.pdf';
 }
@@ -22,12 +22,10 @@ class About {
 
 With experience working on more than 10 production-level apps, I’ve tackled a wide range of real-world features including in-app purchases, socket-based real-time communication, deep links and universal links, and Google Maps integration.
 
-I enjoy the entire process of turning ideas into polished, responsive mobile experiences. From writing clean, maintainable code to optimizing performance and refining UI/UX details, I take pride in delivering apps that feel smooth and intuitive.
+Currently, I’m completing an internship in Go, expanding my backend and full-stack development skills. Alongside Flutter and Dart, I work with tools and technologies like Firebase, REST APIs, and Git to build scalable and reliable applications that deliver real value.
 
-In addition to Flutter and Dart, I work with tools and technologies like Firebase, REST APIs, and Git to build scalable and reliable applications that provide real value.'''
-  ;
+In addition to Flutter and Dart, I work with tools and technologies like Firebase, REST APIs, Go lang and Git to build scalable and reliable applications that provide real value.''';
 }
-
 
 class Experience {}
 
@@ -36,9 +34,9 @@ class Skills {
   static const descriptionHead =
       'The main area of expertise is in mobile application development.';
   static const descriptionPrimary =
-      'I possess strong skills in building high-performance, cross-platform mobile applications using the Flutter framework. I have experience in designing responsive UIs, integrating APIs, managing state with Riverpod, and optimizing app performance.';
+      'I possess strong skills in building high-performance, cross-platform mobile applications using the Flutter framework and backend servers using GO LANG. I have experience in designing responsive UIs, integrating APIs, managing state with Riverpod, provider, GET X, and optimizing app performance.';
   static const descriptionSecondary =
-      "Driven by innovation, I'm always looking to enhance my skills and explore new opportunities in the field of mobile app development.";
+      "Driven by innovation, I'm always looking to enhance my skills and explore new opportunities in the field of mobile app development and building highly scaleable servers.";
 }
 
 class Projects {}
@@ -48,6 +46,6 @@ class Contact {
   static const description =
       "Have a project idea, need a developer, or just want to connect? I'd love to hear from you. Let's create something amazing together!";
   static const descriptionLocation =
-      'Based in Kathmandu, Nepal, open to remote opportunities.';
+      'Based in Karachi, Pakistan, open to remote opportunities.';
   static const headingsSecondary = 'You can reach me at:';
 }
