8cb5b037463e52576e781bc13990007ea90e0c79
taha <tahairtaza121@gmail.com>
2025-12-24 00:38:29 +0500

    Add app icons and launch images for iOS
    - Added various sizes of app icons (20x20, 29x29, 40x40, 60x60, 76x76, 83.5x83.5) in the Assets.xcassets/AppIcon.appiconset directory.
    - Included launch images (LaunchImage.png, LaunchImage@2x.png, LaunchImage@3x.png) in the Assets.xcassets/LaunchImage.imageset directory.
    - Created corresponding Contents.json files for both app icons and launch images to define their properties.
    - Implemented LaunchScreen.storyboard and Main.storyboard for the app's launch and main interface.
    - Updated Info.plist with necessary configurations for the app's display name, versioning, and supported orientations.
    - Added a bridging header for Swift compatibility and included a basic test case in RunnerTests.swift.

--- lib/constants/projects.dart
+++ lib/constants/projects.dart
@@ -14,7 +14,7 @@ final List<Projects> projects = [
     link: 'https://github.com/Tahadxbydy/Portfolio',
   ),
   const Projects(
-      image: 'E2EE Chat App Promotion.png',
+      image: 'remember my love.png',
       title: 'Remember My Love',
       description:
           '''Remember My Love: Capture, Organize, and Send Your Memories to the Future
@@ -32,16 +32,43 @@ In today’s fast-paced world, memories can fade all too quickly. Remember My Lo
       appStoreLink:
           "https://apps.apple.com/us/app/remember-my-love/id6740341709"),
   const Projects(
-    image: 'Document Sharing App Promotion.png',
-    title: 'Document Sharing App',
+    image: 'skinsync Ai.png',
+    title: 'Skin Ai: Under Production',
     description:
-        'Document Sharing App is an Android app built with Java, XML, and Firebase for easy file sharing. Users can log in, create groups to organize files, and upload or manage various file types like PDFs, images, audio, and videos. The app allows file editing, deletion, and downloading directly to Android devices for efficient sharing and management.',
+        '''SkinSync AI is an intelligent beauty and aesthetics app designed to help you visualize your transformation before you take the next step. Using advanced AI and face analysis technology, the app creates realistic before-and-after previews of beauty and cosmetic treatments, allowing you to see how different services could enhance your natural features.
+
+From skin treatments and injectables to facial enhancements, SkinSync AI helps you make confident, informed decisions by showing personalized results based on your own face. Simply capture your photo, explore available beauty services, and instantly preview potential outcomes in a safe, non-invasive way.
+
+In addition to AI-powered previews, the app also connects you with curated beauty services, making it easy to explore options, select treatment areas, and plan your next appointment—all in one place.
+
+SkinSync AI blends technology and beauty to give you clarity, confidence, and control over your aesthetic journey.''',
     builtWith: [
-      'Java',
-      'XML',
+      'Dart',
+      'Flutter',
+      'ML Kit',
       'Firebase',
       'Material UI',
     ],
-    link: 'https://github.com/Tahadxbydy/Document_Sharing_App',
+    link: '',
+    // playStoreLink:
+    //     "https://play.google.com/store/apps/details?id=com.envite.user&hl=en",
+    // appStoreLink: "https://apps.apple.com/us/app/envite/id6738378654"
   ),
+  const Projects(
+      image: 'wajba.png',
+      title: 'Wajba',
+      description:
+          'Every day, restaurants, cafés, and bakeries prepare fresh food that often goes unsold. Wajba gives this food a second chance — connecting you with nearby eateries offering perfectly good meals at a reduced price. Enjoy tasty food, save money, and make a positive impact on the planet. By rescuing unsold meals, you’re helping to reduce food waste and support local businesses. With Wajba, every meal is a win for your taste buds and the environment.',
+      builtWith: [
+        'Dart',
+        'Flutter',
+        'Google Maps',
+        'Firebase',
+        'Material UI',
+      ],
+      link: 'https://github.com/Tahadxbydy/Document_Sharing_App',
+      playStoreLink:
+          "https://play.google.com/store/apps/details?id=com.wajba.app&hl=en",
+      appStoreLink:
+          "https://apps.apple.com/us/app/wajba-save-fresh-meals/id6740336691"),
 ];
